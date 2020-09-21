bba8399ca0efbbb69c4b1f0bc56ffd769b14be63
totyrola <astir.tadrous@polymtl.ca>
2020-09-21 17:51:15 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def use_prefixes() -> List[str]:
     product(prefixes, suffixes, repeat=1)
 
    
-    return product(prefixes,suffixes, repeat=)
+    return product(prefixes,suffixes, repeat=1)
 
 
 def prime_integer_summation() -> int:
