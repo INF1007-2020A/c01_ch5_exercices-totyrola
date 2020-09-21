f64da9744186bbe04dd4ac8c8ee7edf5ed48f51a
totyrola <astir.tadrous@polymtl.ca>
2020-09-21 17:48:55 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,7 @@ def convert_to_absolute() -> float:
 
 def use_prefixes() -> List[str]:
     prefixes, suffixes = 'JKLMNOP', 'ack'
-    product(prefixes, suffixes)
+    product(prefixes, suffixes, repeat)
 
     return [""]
 
