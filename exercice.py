51b787445a59a4407cc5264f4f5fa7901ddb1bfa
totyrola <astir.tadrous@polymtl.ca>
2020-10-01 01:22:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
     resultat = []
     for i in range(len(prefixes))
-        nom = prefixes[i] + suffixe
+    nom = prefixes[i] + suffixe
         resultat.append(nom) 
 
 
